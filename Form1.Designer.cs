228ec4e504f6249562523d6a6a6edc425c473a62
jamie.pate@gmail.com <jamie.pate@gmail.com>
2015-08-08 21:07:15 -0700

    Fix #3 Cannot change settings for individual devices.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -25,97 +25,21 @@
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_showAll = new System.Windows.Forms.Button();
+            this.dgv_flippable = new System.Windows.Forms.DataGridView();
             this.btn_flip = new System.Windows.Forms.Button();
             this.btn_normal = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.Device = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.cbl_names = new System.Windows.Forms.CheckedListBox();
-            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-            this.col_property = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.col_state = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tmr_popup = new System.Windows.Forms.Timer(this.components);
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.verticalDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.horizontalDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.flippableBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_flippable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flippableBindingSource)).BeginInit();
             this.SuspendLayout();
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.btn_flip, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.btn_normal, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.listView1, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.cbl_names, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(588, 376);
-            this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // btn_flip
-            // 
-            this.btn_flip.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn_flip.Location = new System.Drawing.Point(297, 3);
-            this.btn_flip.Name = "btn_flip";
-            this.btn_flip.Size = new System.Drawing.Size(288, 23);
-            this.btn_flip.TabIndex = 0;
-            this.btn_flip.Text = "Flip";
-            this.btn_flip.UseVisualStyleBackColor = true;
-            this.btn_flip.Click += new System.EventHandler(this.btn_flip_Click);
-            // 
-            // btn_normal
-            // 
-            this.btn_normal.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn_normal.Location = new System.Drawing.Point(297, 32);
-            this.btn_normal.Name = "btn_normal";
-            this.btn_normal.Size = new System.Drawing.Size(288, 23);
-            this.btn_normal.TabIndex = 1;
-            this.btn_normal.Text = "Normal";
-            this.btn_normal.UseVisualStyleBackColor = true;
-            this.btn_normal.Click += new System.EventHandler(this.btn_normal_Click);
-            // 
-            // listView1
-            // 
-            this.listView1.Activation = System.Windows.Forms.ItemActivation.OneClick;
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.Device,
-            this.col_property,
-            this.col_state});
-            this.tableLayoutPanel1.SetColumnSpan(this.listView1, 2);
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.HoverSelection = true;
-            this.listView1.Location = new System.Drawing.Point(3, 61);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(582, 312);
-            this.listView1.TabIndex = 2;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // Device
-            // 
-            this.Device.Text = "Device";
-            this.Device.Width = 246;
-            // 
-            // cbl_names
-            // 
-            this.cbl_names.BackColor = System.Drawing.SystemColors.Control;
-            this.cbl_names.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.cbl_names.CheckOnClick = true;
-            this.cbl_names.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.cbl_names.FormattingEnabled = true;
-            this.cbl_names.Items.AddRange(new object[] {
-            "FlipFlopWheel",
-            "FlipFlopHScroll"});
-            this.cbl_names.Location = new System.Drawing.Point(3, 3);
-            this.cbl_names.Name = "cbl_names";
-            this.tableLayoutPanel1.SetRowSpan(this.cbl_names, 2);
-            this.cbl_names.Size = new System.Drawing.Size(288, 52);
-            this.cbl_names.TabIndex = 4;
-            this.cbl_names.ThreeDCheckBoxes = true;
             // 
             // notifyIcon1
             // 
@@ -125,42 +49,144 @@
             this.notifyIcon1.Text = "notifyIcon1";
             this.notifyIcon1.Visible = true;
             // 
-            // col_property
+            // tableLayoutPanel1
             // 
-            this.col_property.Text = "Property";
-            this.col_property.Width = 133;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btn_showAll, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dgv_flippable, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn_flip, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn_normal, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(599, 529);
+            this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // col_state
+            // btn_showAll
             // 
-            this.col_state.Text = "State";
-            this.col_state.Width = 128;
+            this.btn_showAll.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_showAll.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_showAll.Image = ((System.Drawing.Image)(resources.GetObject("btn_showAll.Image")));
+            this.btn_showAll.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_showAll.Location = new System.Drawing.Point(394, 3);
+            this.btn_showAll.Name = "btn_showAll";
+            this.btn_showAll.Size = new System.Drawing.Size(191, 23);
+            this.btn_showAll.TabIndex = 3;
+            this.btn_showAll.Text = "Show All";
+            this.btn_showAll.UseVisualStyleBackColor = true;
+            this.btn_showAll.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgv_flippable
+            // 
+            this.dgv_flippable.AutoGenerateColumns = false;
+            this.dgv_flippable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.verticalDataGridViewTextBoxColumn,
+            this.horizontalDataGridViewTextBoxColumn});
+            this.tableLayoutPanel1.SetColumnSpan(this.dgv_flippable, 4);
+            this.dgv_flippable.DataSource = this.flippableBindingSource;
+            this.dgv_flippable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_flippable.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dgv_flippable.Location = new System.Drawing.Point(3, 32);
+            this.dgv_flippable.Name = "dgv_flippable";
+            this.dgv_flippable.Size = new System.Drawing.Size(593, 494);
+            this.dgv_flippable.TabIndex = 2;
+            this.dgv_flippable.MouseUp += new System.Windows.Forms.MouseEventHandler(this.dgv_flippable_MouseUp);
+            // 
+            // btn_flip
+            // 
+            this.btn_flip.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_flip.Location = new System.Drawing.Point(3, 3);
+            this.btn_flip.Name = "btn_flip";
+            this.btn_flip.Size = new System.Drawing.Size(189, 23);
+            this.btn_flip.TabIndex = 0;
+            this.btn_flip.Text = "Flip All";
+            this.btn_flip.UseVisualStyleBackColor = true;
+            this.btn_flip.Click += new System.EventHandler(this.btn_flip_Click);
+            // 
+            // btn_normal
+            // 
+            this.btn_normal.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_normal.Location = new System.Drawing.Point(198, 3);
+            this.btn_normal.Name = "btn_normal";
+            this.btn_normal.Size = new System.Drawing.Size(190, 23);
+            this.btn_normal.TabIndex = 1;
+            this.btn_normal.Text = "All Normal";
+            this.btn_normal.UseVisualStyleBackColor = true;
+            this.btn_normal.Click += new System.EventHandler(this.btn_normal_Click);
+            // 
+            // tmr_popup
+            // 
+            this.tmr_popup.Interval = 3000;
+            this.tmr_popup.Tick += new System.EventHandler(this.tmr_popup_Tick);
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Device Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // verticalDataGridViewTextBoxColumn
+            // 
+            this.verticalDataGridViewTextBoxColumn.DataPropertyName = "vertical";
+            this.verticalDataGridViewTextBoxColumn.HeaderText = "Vertical Scroll";
+            this.verticalDataGridViewTextBoxColumn.Name = "verticalDataGridViewTextBoxColumn";
+            this.verticalDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.verticalDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.verticalDataGridViewTextBoxColumn.ThreeState = true;
+            // 
+            // horizontalDataGridViewTextBoxColumn
+            // 
+            this.horizontalDataGridViewTextBoxColumn.DataPropertyName = "horizontal";
+            this.horizontalDataGridViewTextBoxColumn.HeaderText = "Horizontal Scroll";
+            this.horizontalDataGridViewTextBoxColumn.Name = "horizontalDataGridViewTextBoxColumn";
+            this.horizontalDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.horizontalDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.horizontalDataGridViewTextBoxColumn.ThreeState = true;
+            // 
+            // flippableBindingSource
+            // 
+            this.flippableBindingSource.DataSource = typeof(WindowsFormsApplication1.Flippable);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(588, 376);
+            this.ClientSize = new System.Drawing.Size(599, 529);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Flip Mouse Wheel";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_flippable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flippableBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dgv_flippable;
         private System.Windows.Forms.Button btn_flip;
         private System.Windows.Forms.Button btn_normal;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader Device;
-        private System.Windows.Forms.NotifyIcon notifyIcon1;
-        private System.Windows.Forms.CheckedListBox cbl_names;
-        private System.Windows.Forms.ColumnHeader col_property;
-        private System.Windows.Forms.ColumnHeader col_state;
+        private System.Windows.Forms.BindingSource flippableBindingSource;
+        private System.Windows.Forms.Button btn_showAll;
+        private System.Windows.Forms.Timer tmr_popup;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn verticalDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn horizontalDataGridViewTextBoxColumn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -11,11 +11,12 @@ using System.Diagnostics;
 using System.Security.Principal;
 
 namespace WindowsFormsApplication1 {
+
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
         }
-
+        private Flippable[] flippable;
         private void Form1_Load(object sender, EventArgs e) {
             WindowsPrincipal pricipal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
             bool hasAdministrativeRight = pricipal.IsInRole(WindowsBuiltInRole.Administrator);
@@ -25,8 +26,14 @@ namespace WindowsFormsApplication1 {
                 this.Close();
                 Application.Exit();
             }
-            for(int i = 0; i < cbl_names.Items.Count;++i) {
-                cbl_names.SetItemChecked(i, true);
+
+            //probably only want to flip mice.
+            flippable = getFlippable("hid.mousedevice");
+            dgv_flippable.DataSource = flippable;
+            foreach (var col in dgv_flippable.Columns.OfType<DataGridViewCheckBoxColumn>()) {
+                col.TrueValue = true;
+                col.FalseValue = false;
+                col.IndeterminateValue = null;
             }
         }
         private static bool RunElevated(string fileName)
@@ -47,17 +54,9 @@ namespace WindowsFormsApplication1 {
             }
             return false;
         }
-        
 
-        private void btn_flip_Click(object sender, EventArgs e) {
-            setFlip(true);
-        }
-
-        private void btn_normal_Click(object sender, EventArgs e) {
-            setFlip(false);
-        }
-
-        private void setFlip(bool p) {
+        private Flippable[] getFlippable(string filter) {
+            List<Flippable> flips = new List<Flippable>();
             using (RegistryKey hid = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Enum\HID\",false)) {
                 foreach (string devicekn in hid.GetSubKeyNames()) {
                     using (RegistryKey device = hid.OpenSubKey(devicekn,false)) {
@@ -65,12 +64,7 @@ namespace WindowsFormsApplication1 {
                             using (RegistryKey device2 = device.OpenSubKey(devicekn2,false)) {
                                 using (RegistryKey devparam = device2.OpenSubKey("Device Parameters",true)) {
                                     if (devparam != null) {
-                                        foreach(string name in cbl_names.CheckedItems.OfType<String>()) {
-                                            if (devparam.GetValue(name, null) != null) {
-                                                devparam.SetValue(name, p ? 1 : 0, RegistryValueKind.DWord);
-                                                listView1.Items.Add((string)device2.GetValue("DeviceDesc")).SubItems.AddRange(new String[]{name,p ? "Flipped" : "Normal"});
-                                            }
-                                        }
+                                        flips.Add(new Flippable(new string[] { devicekn, devicekn2 }, device2, devparam, tmr_popup));
                                     }
                                 }
                             }
@@ -78,10 +72,88 @@ namespace WindowsFormsApplication1 {
                     }
                 }
             }
-            notifyIcon1.ShowBalloonTip(99999999);
-            try {
-                listView1.Items.OfType<ListViewItem>().Last().EnsureVisible();
-            } catch { }
+            if (filter != null) {
+                return flips.Where(f=>f.name.Contains(filter)).ToArray();
+            }
+            return flips.ToArray();
         }
+
+        private void dgv_flippable_MouseUp(object sender, MouseEventArgs e) {
+            dgv_flippable.EndEdit();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            flippable = getFlippable(null);
+            dgv_flippable.DataSource = flippable;
+        }
+
+        private void btn_flip_Click(object sender, EventArgs e) {
+            foreach (var f in flippable) {
+                f.vertical = true;
+                f.horizontal = true;
+            }
+            dgv_flippable.DataSource = null;
+            dgv_flippable.DataSource = flippable;
+        }
+
+        private void btn_normal_Click(object sender, EventArgs e) {
+            foreach (var f in flippable) {
+                f.vertical = false;
+                f.horizontal = false;
+            }
+            dgv_flippable.DataSource = null;
+            dgv_flippable.DataSource = flippable;
+        }
+
+        private void tmr_popup_Tick(object sender, EventArgs e) {
+            tmr_popup.Enabled = false;
+            notifyIcon1.ShowBalloonTip(99999999);
+        }
+    }
+    
+    public class Flippable {
+        public Flippable(string[] keyPath, RegistryKey deviceKey, RegistryKey devparam, Timer timer) {
+            this._keyPath = keyPath;
+            IEnumerable<bool?> flipValues = Flippable.valueNames
+                .Select(v => onlyIntBool(devparam.GetValue(v, null)));
+            this.name = (string)deviceKey.GetValue("DeviceDesc");
+            this._vertical = flipValues.ElementAt(0);
+            this._horizontal = flipValues.ElementAt(1);
+            this._timer = timer;
+        }
+        private bool? onlyIntBool(object value) {
+            try {
+                return value == null ? null : (bool?)(((int)value) != 0);
+            } catch {
+                return null;
+            }
+        }
+        public static string[] valueNames = new string[] { "FlipFlopWheel", "FlipFlopHScroll" };
+
+        public string name { get; private set; }
+        private string[] _keyPath;
+        private bool? _vertical;
+        private bool? _horizontal;
+        Timer _timer;
+        public bool? vertical { set { flip(Flippable.valueNames[0], value); _vertical = value; } get { return _vertical; } }
+        public bool? horizontal { set { flip(Flippable.valueNames[1], value); _horizontal = value; } get { return _horizontal; } }
+
+        public void flip(string valueName, bool? value) {
+            using (RegistryKey hid = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Enum\HID\", false)) {
+                using (RegistryKey device = hid.OpenSubKey(_keyPath[0], false)) {
+                    using (RegistryKey device2 = device.OpenSubKey(_keyPath[1], false)) {
+                        using (RegistryKey devparam = device2.OpenSubKey("Device Parameters", true)) {
+                            if (value == null) {
+                                devparam.DeleteValue(valueName);
+                            } else {
+                                devparam.SetValue(valueName, value == true ? 1 : 0);
+                                _timer.Enabled = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
